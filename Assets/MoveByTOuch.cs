fd8d01bfc15094d6bdc1b7fa0ad7833a8f3d2e6b
boromir <przemekshepard@gmail,com>
2019-04-03 17:39:50 +0200

    adding unity and project files

---
+++ Assets/MoveByTOuch.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveByTOuch : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount>0)
+        {
+            Touch touch0 = Input.GetTouch(0);
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch0.position);
+            touchPosition.z = 0f;
+            transform.position = touch0.position;
+
+
+        }
+    }
+}
